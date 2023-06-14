9948c3aa9733ffde19c72c76d7e7097be3a266a5
FerchoKD <213524@ids.upchiapas.edu.mx>
2023-06-13 20:40:41 -0600

    initial commit

---
+++ src/main/java/com/reto/GestionDeTareas/GestionDeTareasApplication.java
@@ -0,0 +1,13 @@
+package com.reto.GestionDeTareas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDeTareasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDeTareasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reto/GestionDeTareas/GestionDeTareasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reto.GestionDeTareas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDeTareasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
